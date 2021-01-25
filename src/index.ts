f657ce3d8a9ff21d830f7192908a9716e6a2df25
Palash Sureka <pal.palashsureka@gmail.com>
2021-01-25 11:58:07 +0530

    Set to use EJS

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use('/js',  express.static(path.resolve(__dirname, 'assets/js')))
 
 
 app.get('/' , (req, res) => {
-    res.send('Hello World!')
+    res.render('index')
     console.log(req.baseUrl)
 })
 
